c845c4aeed58e97b92259ae5bcddb8e7d7fe763d
Nash Luffman <nashluffman@gmail.com>
2019-02-18 14:50:30 -0500

    Added core back in for example

--- FrontEnd/DjangoExample/Webpage/urls.py
+++ FrontEnd/DjangoExample/Webpage/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.urls import path
 
 urlpatterns = [
 
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     #path('polls/', include('polls.urls')),
     #path('core/', include('core.urls')),
     url(r'^',include('fileupload.urls')),
