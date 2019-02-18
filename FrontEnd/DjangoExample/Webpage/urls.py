8b0721d077fc9da7e41e2ba729ce16844d4cd1af
Nash Luffman <nashluffman@gmail.com>
2019-02-17 22:32:36 -0500

    Began development of Django frontend

--- FrontEnd/DjangoExample/Webpage/urls.py
+++ FrontEnd/DjangoExample/Webpage/urls.py
@@ -1,4 +1,4 @@
-"""SkyAudio URL Configuration
+"""Webpage URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
 ]
