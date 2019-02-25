62429aae10c9a21b2260af88009a94514cc3fd37
Nash Luffman <nashluffman@gmail.com>
2019-02-24 21:41:20 -0500

    Import and export files from S3 bucket

--- BackEnd/ImportExport/importFile.py
+++ BackEnd/ImportExport/importFile.py
@@ -18,43 +18,67 @@
 #sudo pip install pygobject
 #sudo pip install gstreamer
 #sudo pip install audioread
-
+#sudo pip install boto3
 
 import sys
 import os
 import audioread
 import contextlib
 import wave
+import boto3
+import botocore
 
-def decode(filename):
-    filename = os.path.abspath(os.path.expanduser(filename))
-    if not os.path.exists(filename):
+#Create S3 resource & define properties
+s3 = boto3.resource('s3')
+BUCKET_NAME = 'django-file-storage'
+
+#Pulling file from AWS S3 Bucket
+def bucketPull(file_name):
+    try:
+        s3.Bucket(BUCKET_NAME).download_file('RawFiles/{}'.format(file_name), '/tmp/' + file_name) 
+        print("File downloaded to tmp directory succesfully!")
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+
+#Pushing the converted file back to the bucket
+def bucketPush(file_name):
+    try:
+        s3.Bucket(BUCKET_NAME).upload_file(('/tmp/' + file_name  + '.wav'), 'ConvertedFiles/{}'.format(file_name + '.wav')) #Upload call
+        print("File uploaded successfully!")
+    finally:
+        os.remove('/tmp/' + file_name + '.wav') #Delete converted file from tmp directory
+        os.remove('/tmp/' + file_name) #Delete converted
+
+#Convert file
+def decode(file_name):
+    file_location = os.path.abspath(os.path.expanduser('/tmp/' + file_name))
+    if not os.path.exists(file_location):
         print("File not found. Please check path.", file=sys.stderr)
         sys.exit(1)
-
     try:
-        with audioread.audio_open(filename) as f:
+        with audioread.audio_open(file_location) as f:
             print('Input file: %i channels at %i Hz; %.1f seconds.' %
                   (f.channels, f.samplerate, f.duration),
                   file=sys.stderr)
             print('Backend:', str(type(f).__module__).split('.')[1],
                   file=sys.stderr)
 
-            with contextlib.closing(wave.open(filename + '.wav', 'w')) as of:
+            with contextlib.closing(wave.open(file_location + '.wav', 'w')) as of:
                 of.setnchannels(f.channels)
                 of.setframerate(f.samplerate)
                 of.setsampwidth(2)
 
                 for buf in f:
                     of.writeframes(buf)
-
     except audioread.DecodeError:
         print("File could not be decoded.", file=sys.stderr)
         sys.exit(1)
 
-
-
-
-
 if __name__ == '__main__':
-   decode("/home/nash/Code/SkyAudio/SampleFiles/Sample.aif")
+    file_name=input("Enter the file name with extension: ")
+    bucketPull(file_name)
+    decode(file_name)
+    bucketPush(file_name)
