a46121306895598b6281bea96007ce102d981bf2
Nash Luffman <nashluffman@gmail.com>
2019-03-17 21:12:41 -0400

    Further exploration of js callbacks

--- FrontEnd/Flask_Blog/static/recordAudio.js
+++ FrontEnd/Flask_Blog/static/recordAudio.js
@@ -2,41 +2,47 @@ var stopButton = document.getElementById('stop_button');
 var startButton = document.getElementById('start_button');
 
 //Gets audio permission from browser & creates media stream if successful
-function getPermission(){
-   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) { //Checking for getUserMedia support
-      console.log('getUserMedia supported.'); 
+ 
+//function getPermission(){
+   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      console.log('getUserMedia supported.');
       navigator.mediaDevices.getUserMedia (
+         // constraints - only audio needed for this app
          {
             audio: true
          })
-
-         // Success callback
-         .then(function(stream) { //Successful creation of a media stream
-               var mR = new MediaRecorder(stream);
-               console.log("Hit me");
-               console.log("Stream & MediaRecorder created successfully");
-               return mR;
-         })
-
-         // Error callback
-         .catch(function(err) {
-            console.log('The following getUserMedia error occured: ' + err);
-         }
-      );
    } else {
       console.log('getUserMedia not supported on your browser!');
    }
-     
-}
+
+//var onSuccess = function(stream){
+   //var mediaRecorder = new MediaRecorder(stream); 
+//   startButton.onclick = function(){startRecording()};
+//   stopButton.onclick = function(){stopRecording()};
+
+//}
 //Creates new media recorder instance and passes it the stream directly.
 //At this point, the stream is ready to be captured into a Blob (file-like object of immutable, raw data)
 
+initializeRecorder = function(){
+   var audioStream = stream;
+   var mediaRecorder = new MediaRecorder(audioStream);
+   console.log(mediaRecorder.state);
+}
+stopButton.onclick = function(){
+   mediaRecorder.stop();
+   console.log("Recording stopped")
+};
 
-startButton.onclick = function(){startRecording()};
-stopButton.onclick = function(){stopRecording()};
+startButton.onclick = function(){
+   initializeRecorder();
+   console.log("Recording started");
+};
 
-async function startRecording(){
-      var mediaRecorder = await getPermission();
+
+
+function startRecording(){
+
       mediaRecorder.start();
       console.log(mediaRecorder.state);
       console.log("Recording started");
