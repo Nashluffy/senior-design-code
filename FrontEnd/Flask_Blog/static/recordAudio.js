676dd10313bdd12af9b0da56c5794e5cb40fe8ee
Nash Luffman <nashluffman@gmail.com>
2019-03-18 08:35:55 -0400

    Successfully implemented string and media recording, struggling with scope issues

--- FrontEnd/Flask_Blog/static/recordAudio.js
+++ FrontEnd/Flask_Blog/static/recordAudio.js
@@ -1,54 +1,42 @@
 var stopButton = document.getElementById('stop_button');
 var startButton = document.getElementById('start_button');
+stopButton.disabled = true; //Disable stopping recording when not recording in the first place
+//Gets audio permission from browser & creates a new MediaStream on success
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+   console.log('getUserMedia supported.');
+   // constraints - only audio needed for this app
+   var constraints = 
+      {
+         audio: true
+      }
+   var onSuccess = function(stream){
+      var mediaRecorder = new MediaRecorder(mediaStream); 
 
-//Gets audio permission from browser & creates media stream if successful
- 
-//function getPermission(){
-   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      console.log('getUserMedia supported.');
-      navigator.mediaDevices.getUserMedia (
-         // constraints - only audio needed for this app
-         {
-            audio: true
-         })
-   } else {
+      startButton.onclick = function(){
+         console.log("hit me")
+         stopButton.disabled = false; //Enable stopping now that we are recording
+         mediaRecorder.start();
+         console.log("Media Recorder status: " + mediaRecorder.active);
+      }
+   
+      stopButton.onclick = function(){
+         mediaRecorder.stop();
+         console.log("Recording stopped")
+      }
+   }
+   var onError = function(err){console.log("Something went wrong!");}
+   navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+   }
+   else {
       console.log('getUserMedia not supported on your browser!');
    }
-
-//var onSuccess = function(stream){
-   //var mediaRecorder = new MediaRecorder(stream); 
-//   startButton.onclick = function(){startRecording()};
-//   stopButton.onclick = function(){stopRecording()};
-
 //}
 //Creates new media recorder instance and passes it the stream directly.
 //At this point, the stream is ready to be captured into a Blob (file-like object of immutable, raw data)
 
-initializeRecorder = function(){
-   var audioStream = stream;
-   var mediaRecorder = new MediaRecorder(audioStream);
-   console.log(mediaRecorder.state);
-}
-stopButton.onclick = function(){
-   mediaRecorder.stop();
-   console.log("Recording stopped")
-};
-
 startButton.onclick = function(){
-   initializeRecorder();
-   console.log("Recording started");
-};
-
-
-
-function startRecording(){
-
-      mediaRecorder.start();
-      console.log(mediaRecorder.state);
-      console.log("Recording started");
-}   
-
-function stopRecording(){
-  mediaRecorder.stop();
-  console.log("Recording stopped");
-}       
+   console.log("hit me")
+   stopButton.disabled = false; //Enable stopping now that we are recording
+   mediaRecorder.start();
+   console.log("Media Recorder status: " + mediaRecorder.active);
+}
