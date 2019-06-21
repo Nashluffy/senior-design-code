4f0de32687e9ce0ef9680aa8e946f30fea7b22a0
root <root@ip-172-31-88-252.ec2.internal>
2019-06-21 00:40:08 +0000

    put all master and reverb in individual files

---
+++ BackEnd/SignalP/reverb.py
@@ -0,0 +1,49 @@
+from pysndfx import AudioEffectsChain
+#Reverb takes 7 parameters:
+    #Reverberance
+    #High-Frequency Damping
+    #Room Scale
+    #Stereo Depth
+    #Pre-Delay
+    #Wet Gain
+    #Wet Only
+
+inf = '~/SkyAudio/SkyAudio/SampleFiles/Sample.wav'
+outf = '~/SkyAudio/SkyAudio/SampleFiles/reverb'
+
+def SmallRoom():
+    try:
+        fx = (AudioEffectsChain().reverb(34, 60, 20, 100, 20, 0, False))
+        outf = outf + 'SmallRoom.wav'
+        pysndfx.fx(inf, outf)
+        print('Successfully applied small room!')
+    except:
+        print ('Something wrong with SoX')
+
+def ReflectiveRoom():
+    try:
+        fx = (AudioEffectsChain().reverb(80, 10, 10, 100, 40, 4, False).normalize())
+        outf = outf + 'ReflectiveRoom.wav'
+        pysndfx.fx(inf, outf)
+        print('Sucessfully applied reflective room!')
+    except:
+        print('Something wrong with SoX')
+
+def ReflectiveCave():
+    try:
+        fx = (AudioEffectsChain().reverb(100, 50, 100, 100, 50, 0, False))
+        outf = outf + 'ReflectiveCave.wav'
+        pysndfx.fx(inf, outf)
+        print('Successfully applied reflective cave!')
+    except:
+        print('Something wrong with SoX')
+def BigRoom():
+    try:
+        fx = (AudioEffectsChain().reverb(80, 75, 100, 50, 20, 0, False))
+        outf = outf + 'BigHall.wav'
+        pysndfx.fx(inf, outf)
+        print('Sucessfully applied big hall!')
+    except:
+        print('Something wrong with SoX')
+
+
