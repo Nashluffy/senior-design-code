d49a9d4a784a955a0ee469b451b2c455af5db95d
twantheswan <twantheswan@gmail.com>
2019-09-22 17:14:58 -0400

    Got reverb.py to work locally. Changed code up and added comments with what needs to be changed.

--- BackEnd/SignalP/reverb.py
+++ BackEnd/SignalP/reverb.py
@@ -1,6 +1,10 @@
-import pysndfx
-from pysndfx import AudioEffectsChain
+#this import below needs to either specify the pysndfx location
+#like: 
+#from directoryHome.libraries.python-audio-effect-master.pysndfx import AudioEffectsChain
+#I'm not sure where the libraries are located within the virtual env so nash will have to test it
+#OR you can just have the python files inside of the directory.
 
+from pysndfx import AudioEffectsChain
 #Reverb takes 7 parameters:
     #Reverberance
     #High-Frequency Damping
@@ -10,31 +14,27 @@ from pysndfx import AudioEffectsChain
     #Wet Gain
     #Wet Only
 
-inf = '/home/ec2-user/SkyAudio/SkyAudio/FrontEnd/Flask/tmp/download.wav'
-outf = '/home/ec2-user/SkyAudio/SkyAudio/FrontEnd/Flask/tmp/processed.wav'
 
-def SmallRoom():
+
+#The binary error came from how we were calling the input and output file
+inf = '~/SkyAudio/SkyAudio/SampleFiles/Sample.wav'
+outf = '~/SkyAudio/SkyAudio/SampleFiles/reverb'
+
+def SmallRoom(waveform):
     try:
-        fx = (
-             AudioEffectsChain()
-             .reverb(34, 60, 20, 100, 20, 0, False)
-        )
-        fx(inf, outf)
-        print('Successfully applied small room!')
+        fx = (AudioEffectsChain().reverb(34, 60, 20, 100, 20, 0, False))
+        outf = outf + 'SmallRoom.wav'
+        #I took out the pysndfx.fx call and just called the function directly
+        fx(waveform, outf)
         return 'Successfully applied small room!'
-    except Exception as e:
-        print(e)
-        return e
-
-def TestMe():
-     print('Reverb hit')
-     return ('Hit me')
+    except:
+        print ('Something wrong with SoX')
 
 def ReflectiveRoom():
     try:
         fx = (AudioEffectsChain().reverb(80, 10, 10, 100, 40, 4, False).normalize())
         outf = outf + 'ReflectiveRoom.wav'
-        pysndfx.fx(inf, outf)
+        fx(inf, outf)
         print('Sucessfully applied reflective room!')
     except:
         print('Something wrong with SoX')
@@ -43,7 +43,7 @@ def ReflectiveCave():
     try:
         fx = (AudioEffectsChain().reverb(100, 50, 100, 100, 50, 0, False))
         outf = outf + 'ReflectiveCave.wav'
-        pysndfx.fx(inf, outf)
+        fx(inf, outf)
         print('Successfully applied reflective cave!')
     except:
         print('Something wrong with SoX')
@@ -51,8 +51,7 @@ def BigRoom():
     try:
         fx = (AudioEffectsChain().reverb(80, 75, 100, 50, 20, 0, False))
         outf = outf + 'BigHall.wav'
-        pysndfx.fx(inf, outf)
+        fx(inf, outf)
         print('Sucessfully applied big hall!')
     except:
         print('Something wrong with SoX')
-
