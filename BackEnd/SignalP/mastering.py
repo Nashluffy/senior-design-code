e742c31fb686de98d7db50f3a86d50a6d1f63a7a
Nash Luffman <nashluffman@gmail.com>
2019-09-28 21:51:37 -0400

    normalized signalp functions

--- BackEnd/SignalP/mastering.py
+++ BackEnd/SignalP/mastering.py
@@ -8,22 +8,16 @@ from pysndfx import AudioEffectsChain
     #Wet Gain
     #Wet Only
 
-inf = 'test.wav'
-outf = 'outputMastering.wav'
+inf = 'files/test.wav'
+outf = 'files/output'
 
 def SmallRoom():
     try:
-        fx = (
-            AudioEffectsChain()
-            .equalizer(10000, 1, 3)
-            .equalizer(20, 1, -10)
-            .limiter(3)
-            .normalize()
-            .compand(0.2, 1, 2.0, -20, -20, -20)
-        )
-        outf = outf + 'Simple.wav'
+        fx = (AudioEffectsChain().equalizer(10000, 1, 3).equalizer(20, 1, -10).limiter(3).normalize().compand(0.2, 1, 2.0, -20, -20, -20))
+        outf = outf + 'SmallRoom.wav'
         fx(inf, outf)
         print('Successfully applied simple mastering!')
-    except:
-        print ('Something wrong with SoX')
+    except Exception as e:
+        print(e)
+        return e
 
