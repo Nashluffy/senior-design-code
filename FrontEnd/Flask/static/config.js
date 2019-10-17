51f51543ae65dd1f273ff672c89f762b1fa0b344
pressure101 <cmorri89@uncc.edu>
2019-10-17 10:33:30 -0400

    added config file

---
+++ FrontEnd/Flask/static/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_jlssO4wrF', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '6apv44v66acs9ggl2cja6ms2tg' //is this used anywhere?
+    },
+};
